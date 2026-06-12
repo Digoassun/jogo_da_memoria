7ef7d1b0b1f8471809daccbf7e6d1b8c7b88485b
rodrigo.larangeiras <rodrigo.gaglianone@exporj.com>
2026-06-11 23:04:02 -0300

    feat: logica do jogo completa

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,13 +1,14 @@
-import Game from "@/page/Game.vue";
-import Home from "@/page/Home.vue";
+import Game from '@/page/Game.vue'
+import Home from '@/page/Home.vue'
+import type { RouteRecordRaw } from 'vue-router'
 
-export const routes = [
-    {
-        path: '/',
-        component:Home
-    },
-    {
-        path:'/game',
-        component: Game
-    }
+export const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: Home,
+  },
+  {
+    path: '/game',
+    component: Game,
+  },
 ]
